627bb14435c3244ba9d2521c1f98f6395bdd0ac6
xt8736 <xt8736@rimac.com.pe>
2025-02-07 15:54:47 -0500

    feat: started project

---
+++ src/main/java/com/seguro/ProcesarReclamoSegurosApplication.java
@@ -0,0 +1,13 @@
+package com.seguro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcesarReclamoSegurosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcesarReclamoSegurosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seguro/ProcesarReclamoSegurosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seguro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcesarReclamoSegurosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
